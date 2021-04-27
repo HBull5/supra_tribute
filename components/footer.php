8621b8585e1588b5d2d6f6590b5e9d3031317464
Nick Bull <harcourtbullv@gmail.com>
2021-04-27 16:10:21 -0400

    ok footer complete

---
+++ components/footer.php
@@ -0,0 +1,12 @@
+<footer class="text-white w-full bg-red-600 flex">
+    <div class="container mx-auto flex flex-col justify-center items-center text-center">
+        <h3 class="roboto-thick text-2xl mb-5">Supra Tribute</h3>
+        <ul class="flex mb-5">
+            <li class="mx-5"><a href="/">Home</a></li>
+            <li class="mx-5"><a href="/gallery.php">Gallery</a></li>
+            <li class="mx-5"><a href="/models.php">Models</a></li>
+            <li class="mx-5"><a href="/history.php">History</a></li>
+        </ul>
+        <p class="mb-5">Harcourt Web Designs &copy; 2021</p>
+    </div>
+</footer>
